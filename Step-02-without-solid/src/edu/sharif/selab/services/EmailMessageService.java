6b640369e1fc874aac59beb917023e7aa23ba23f
Global Village <keihanikiana@gmail.com>
2024-11-01 22:48:23 +0330

    add changes to emailMessageService

--- Step-02-without-solid/src/edu/sharif/selab/services/EmailMessageService.java
+++ Step-02-without-solid/src/edu/sharif/selab/services/EmailMessageService.java
@@ -1,39 +1,27 @@
 package edu.sharif.selab.services;
 
+import edu.sharif.selab.models.Message;
 import edu.sharif.selab.models.EmailMessage;
-import edu.sharif.selab.models.SmsMessage;
-import edu.sharif.selab.models.TelegramMessage;
 
 import java.util.regex.Pattern;
 
-public class EmailMessageService implements MessageService{
+public class EmailMessageService implements MessageService {
     @Override
-    public void sendSmsMessage(SmsMessage smsMessage) {
-        //Empty Body
-    }
-
-    @Override
-    public void sendEmailMessage(EmailMessage emailMessage) {
-        if(validateEmailAddress(emailMessage.getSourceEmailAddress()) && validateEmailAddress(emailMessage.getTargetEmailAddress())){
-            System.out.println("Sending a SMS from " + emailMessage.getSourceEmailAddress() + " to " + emailMessage.getTargetEmailAddress() + " with content : " + emailMessage.getContent());
-        }else{
-            throw new IllegalArgumentException("Email Address is Not Correct!");
+    public void sendMessage(Message message) {
+        if (message instanceof EmailMessage) {
+            EmailMessage emailMessage = (EmailMessage) message;
+            if (validateReceiverAddress(emailMessage.getTargetEmailAddress())) {
+                System.out.println("Sending an Email from " + emailMessage.getSourceEmailAddress() + " to " + emailMessage.getTargetEmailAddress() + " with content: " + emailMessage.getContent());
+            } else {
+                throw new IllegalArgumentException("Invalid email address!");
+            }
         }
     }
 
     @Override
-    public void sendTelegramMessage(TelegramMessage telegramMessage) {
-
-    }
-
-    public boolean validateEmailAddress(String email) {
+    public boolean validateReceiverAddress(String address) {
         // Regular expression pattern for validating email addresses
         String emailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-
-        // Compile the pattern into a regex Pattern object
-        Pattern pattern = Pattern.compile(emailRegex);
-
-        // Check if the email string matches the regex pattern
-        return pattern.matcher(email).matches();
+        return Pattern.compile(emailRegex).matcher(address).matches();
     }
 }
