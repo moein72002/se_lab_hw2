55d9a4187d09d14ca651a21b233169ae9b9595c0
Moein Madadi <moeinmadadi2002@gmail.com>
2024-11-01 17:54:12 +0330

    add initial project

---
+++ MessageSendingPractice/src/edu/sharif/selab/services/TelegramMessageService.java
@@ -0,0 +1,16 @@
+package edu.sharif.selab.services;
+
+import edu.sharif.selab.models.EmailMessage;
+import edu.sharif.selab.models.SmsMessage;
+
+public class TelegramMessageService implements MessageService{
+    @Override
+    public void sendSmsMessage(SmsMessage smsMessage) {
+
+    }
+
+    @Override
+    public void sendEmailMessage(EmailMessage emailMessage) {
+
+    }
+}
